d9acac2c608442056a9b259f49f29a946ece32d8
BheemRaj <bheemraj8639@gmail.com>
2025-04-29 16:41:34 -0500

    form-based-authentication

---
+++ src/main/java/com/crudProduct/thymeleaf/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.crudProduct.thymeleaf.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/register", "/login", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").defaultSuccessUrl("/home", true).permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout").permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
